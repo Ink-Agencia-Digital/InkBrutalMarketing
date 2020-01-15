7c5df583a40f7cd4ac9a4549418b00bc156a5aca
YonathanInk <58703131+YonathanInk@users.noreply.github.com>
2020-01-15 18:00:15 -0500

    Crear empresa funcionando

--- frontend/src/views/Empresa.vue
+++ frontend/src/views/Empresa.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="container">
     <div class="col-md-8">
-      <form>
+      <form @submit.prevent="onSubmit">
         <br /><br />
         <h1>Empresa</h1>
         <br />
         <div class="form-row">
           <div class="form-group col-md-6">
             <label for="empresa">Empresa</label>
-            <input type="text" class="form-control" id="empresa" />
+            <input
+              type="text"
+              class="form-control"
+              v-model="EMP_Nombre_Empresa"
+              placeholder="Nombre Empresa"
+            />
           </div>
           <div class="form-group col-md-6">
             <label for="tipo">Tipo empresa</label>
@@ -61,6 +66,7 @@
             <input type="text" class="form-control" id="cargo" />
           </div>
         </div>
+        <p v-if="error" class="muted mt-2" style="color: red;">{{ error }}</p>
         <br />
         <button
           type="submit"
@@ -74,6 +80,36 @@
     </div>
   </div>
 </template>
+
+<script>
+import { apiService } from "@/common/api.service.js";
+export default {
+  data() {
+    return {
+      error: null
+    };
+  },
+  methods: {
+    onSubmit() {
+      if(!this.EMP_Nombre_Empresa) {
+        this.error = "Por favor digite un nombre de empresa";
+      } else if (this.EMP_Nombre_Empresa.length > 100) {
+        this.error = "El nombre de la empresa no puede ser superior a 100 caracteres";
+      } else {
+        let endpoint = "/api/empresa/";
+        let method = "POST";
+        apiService(endpoint, method, { EMP_Nombre_Empresa: this.EMP_Nombre_Empresa })
+          .then(() => {
+          this.$router.push({
+            name: "principal_empresa"
+          });
+        });
+      }
+    }
+  }
+};
+</script>
+
 <style>
 .background {
   position: absolute;
